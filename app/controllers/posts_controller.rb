ff503028a5e35d7688b7c79414052cfd5eda9e5e
tzvaita <tzvaita@gmail.com>
2020-04-21 17:28:31 +0200

    Added full authentication for logged in users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,18 +2,19 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @post = Post.all
+    @posts = Post.all.order("created_at DESC")
+    @post = Post.new
   end
 
   def show
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
     @post.user_id = current_user.id
     if @post.save
       flash[:success] = "You've posted successfully!"
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'posts#index'
   get 'posts/show'
-  get 'posts/new'
-  devise_for :users
+  get '/new_post', to: 'posts#new'
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   resources :posts, only: [:new, :create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
